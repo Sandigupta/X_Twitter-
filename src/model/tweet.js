6308d8468a79c15d8f4b2cf8beaf3d047b7bab47
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2025-06-02 03:12:33 +0530

    Added v1 for tweet model and hashtag model

--- src/model/tweet.js
+++ src/model/tweet.js
@@ -15,17 +15,6 @@ const tweetSchema = new mongoose.Schema({
    
 }, { timestamps: true });
 
-// Defines a virtual property 'contentWithEmail' that combines the tweet content with the creator's email.
-// Virtuals are not stored in the database — they are computed properties available when using .toObject() or .toJSON().
-tweetSchema.virtual("contentWithEmail").get(function () {
-    return this.content + "\n Created by: " + this.userEmail;
-})
-
-tweetSchema.pre('save', function (next) {
-    console.log("Inside the Hook");
-    // this.content=this.content+"....."
-    
-})
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
